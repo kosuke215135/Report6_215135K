e1e88a0d0c379c08cc08285c77773732cf941ca9
kosuke215135 <e215135@ie.u-ryukyu.ac.jp>
2023-01-04 10:15:16 +0900

    make PlayerTest

--- app/src/test/java/jp/ac/uryukyu/ie/e215135/PlayerTest.java
+++ app/src/test/java/jp/ac/uryukyu/ie/e215135/PlayerTest.java
@@ -5,9 +5,30 @@ package jp.ac.uryukyu.ie.e215135;
 
 import org.junit.jupiter.api.Test;
 import static org.junit.jupiter.api.Assertions.*;
+import java.util.ArrayList;
+import java.util.List;
 
-// class MainTest {
-//     @Test void appHasAGreeting() {
-//         null;
-//     }
-// }
+class PlayerTest {
+    @Test void checkPlayer() {
+        //calcNum()
+        Player player = new Player();
+        Card card1 = new Card(2, "♤");
+        Card card2 = new Card(5, "♡");
+        player.setCard(card1);
+        player.setCard(card2);
+        List<Integer> listA = new ArrayList<Integer>();
+        int ans = player.calcNum(listA);
+        assertEquals(7,ans);
+        
+        // calcMinNum
+        Card card3 = new Card(1, "♢");
+        player.setCard(card3);
+        ans = player.calcMinNum();
+        assertEquals(8,ans);
+        
+        // Aを11で計算するとき
+        listA.add(2);
+        ans = player.calcNum(listA);
+        assertEquals(18,ans);
+    }
+}
